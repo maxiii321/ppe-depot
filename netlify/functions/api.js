649412717ac3acdcd6a956b09fbecdd2cbc7c644
Your Name <your.email@example.com>
2026-09-19 11:15:19 +0400

    Add @netlify/plugin-express to devDependencies

--- netlify/functions/api.js
+++
@@ -1,5 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../../server.js');
-
-// Export the serverless function handler required by Netlify
-module.exports.handler = serverless(app);
